09f33db36274559d7e5da5d117e9ac08aa6d13fe
Yizirui Fang <circletree@outlook.com>
2022-02-08 18:48:04 +0800

    feat: result analysis

---
+++ analysis/risk_model_analysis.py
@@ -0,0 +1,24 @@
+import pickle
+import os
+import matplotlib.pyplot as plt
+
+
+def random_model_aggregate():
+    dir = '/workspace/FYP/codespace/output/random/'
+    roc_list = []
+    x = list(range(2006, 2020))
+    num = 0
+    for file in os.listdir(dir):
+        f = open(dir + file, 'rb')
+        result = pickle.load(f)
+        roc_list.append(result['all year AUC'][:14])
+        plt.plot(x, result['all year AUC'][:14])
+        num = num + 1
+        print(file)
+    plt.title("AUC of Gradient Boosting - " + str(num) + " Times")
+    plt.xlabel("Year")
+    plt.ylabel("AUC")
+    plt.show()
+
+if __name__ == '__main__':
+    random_model_aggregate()
