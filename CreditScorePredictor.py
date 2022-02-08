b9c9ae4ad42ce200f86ec92a5f149348666c5c4c
Yizirui Fang <circletree@outlook.com>
2022-02-08 18:44:13 +0800

    feat: missing value predictors

---
+++ CreditScorePredictor.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LogisticRegression
+
+
+class CreditScoreRegressionClassifier:
+    def __init__(self, random_state=0):
+        self.random_state = random_state
+        self.classifier = LogisticRegression(random_state=random_state)
+
+    def train(self, origination_x):
+        X = origination_x[:, 1:]
+        y = origination_x[:, 0].astype('int')
+        self.classifier.fit(X, y)
+
+    def predict(self, X):
+        return self.classifier.predict(X)
