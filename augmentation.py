558337e0ee48b72ee58845b9e0832020bf4000b1
Yizirui Fang <circletree@outlook.com>
2022-05-24 23:13:14 +0800

    feat: augmentation approaches

--- augmentation.py
+++ augmentation.py
@@ -1,6 +1,5 @@
 import math
 
-import self as self
 from scipy import stats
 import numpy as np
 import statsmodels.api as sm
@@ -13,20 +12,22 @@ from KDEpy import NaiveKDE, TreeKDE, FFTKDE
 def distribution_normalization(input):
     data_std = np.std(input, axis=0)
     data_mean = np.mean(input, axis=0)
-    norm = (input - data_mean) / (data_std + 1e-4) + 1e-4
+    norm = (input - data_mean) / (data_std + 1e-32) + 1e-32
     return norm
 
 
-def distribution_modifier(data_set):
+def distribution_modifier(data_set, __bandwidth=1):
     data_sum_min = np.min(data_set, axis=0)
     data_sum_max = np.max(data_set, axis=0)
     # percentage_x = (data_set[0] - data_sum_min) / (data_sum_max - data_sum_min+ 1e-4)
     norm_x = distribution_normalization(data_set)
     # kernel = stats.gaussian_kde(np.unique(integrated_data.T, axis=1), bw_method=1)
     # KDE Method 1
-    dens = sm.nonparametric.KDEMultivariate(norm_x, var_type='c' * norm_x.shape[1], bw=np.repeat(0.4, norm_x.shape[1]))
+    # dens = sm.nonparametric.KDEMultivariate(norm_x, var_type='c' * norm_x.shape[1],
+    #                                         bw=np.repeat(__bandwidth, norm_x.shape[1]))
+    dens = None
     # KDE Method 2
-    naive_kde = TreeKDE(kernel='gaussian', bw=0.4).fit(norm_x)
+    naive_kde = NaiveKDE(kernel='gaussian', bw=__bandwidth).fit(norm_x)
     return dens, naive_kde
 
 
